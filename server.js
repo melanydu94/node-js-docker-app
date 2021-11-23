1256cca7d69f543ee2ae58c45d9d00d24c1b447d
melanydu94 <63240176+melanydu94@users.noreply.github.com>
2021-11-23 18:35:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const port = 8080
 const host = '0.0.0.0';
 
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
 const mysqlConfig = {
     host: "mysql_server",
